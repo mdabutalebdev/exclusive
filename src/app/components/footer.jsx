4e9a438e2b87ff53444cee2a6a8f372ad6a2b6e3
mdabutalebdev <mdabutaleb.dev@gmail.com>
2025-05-11 16:24:08 +0600

    this is footer

---
+++ src/app/components/footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>Footer
+        <p>© 2023 Your Company Name. All rights reserved.</p>
+        <ul>
+            <li><a href="/privacy">Privacy Policy</a></li>
+            <li><a href="/terms">Terms of Service</a></li>
+            <li><a href="/contact">Contact Us</a></li>
+        </ul>
+    </div>
+  )
+}
+
+export default Footer
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
-import Image from "next/image";
+// This is a React component that serves as the main page of the application.
+import Footer from "./components/footer";
 import Navbar from "./components/navbar";
 
 export default function Home() {
   return (
      <>
      <Navbar/>
+     <Footer/>
      </>
   );
 }
