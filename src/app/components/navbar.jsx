c57c10ef73bb2b6b64572a8a0a8f1e1f00c25c85
mdabutalebdev <mdabutaleb.dev@gmail.com>
2025-05-14 16:39:28 +0600

    this is nav

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <section className="navbar">
+      <div className="navbar__logo">
+        <h1>My Website</h1>
+      </div>
+      <nav className="navbar__links">
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/services">Services</a></li>
+          <li><a href="/contact">Contact</a></li>
+        </ul>
+      </nav>
+      </section>
   )
 }
 
